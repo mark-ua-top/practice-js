d924791f02dbc64abc8028d50e76d648bdbfe422
MarkUA <mark.d.nahorniak@gmail.com>
2024-09-30 21:02:17 +0300

    fix

--- js/test.js
+++
@@ -1,7 +0,0 @@
-// console.log("Hello, World!");
-// console.log(7 * 8);  
-// "javascript is awesome".length;
-// "javascript is awesome".toUpperCase();
-
-// const myHobby = "music"
-// console.log(myHobby)

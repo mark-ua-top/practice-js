9ad46909da37a38de9816c41f6929de5bb61bd27
MarkUA <mark.d.nahorniak@gmail.com>
2024-10-08 22:36:18 +0300

    fix

---
+++ js/scopeVariables.js
@@ -0,0 +1,27 @@
+// зона видимості
+
+// // глобальна зона видимості
+// const d = 10;
+// console.log(d)
+
+// // блочна або локальна зона видимості
+// if (true) {
+// const a = 5
+// console.log(a)
+// }
+
+// // for (const iterator of object) {
+// //     const a = 3
+// //     console.log(a)
+// // }
+
+// function test () {
+//     const a = 7
+//     console.log(a)
+
+//     if (5 > 4) {
+//         console.log(d)
+//     }
+// }
+
+// test()

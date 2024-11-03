625e7c55aaf8f2dfea3a3e1619ea2dcbfdfba98c
MarkUA <mark.d.nahorniak@gmail.com>
2024-11-03 11:52:38 +0200

    fix

---
+++ js/aray-metods.js
@@ -0,0 +1,41 @@
+//split метод робить з рядка масив по роздільнику (деліметру)
+// const splitMessage = "I love induk"
+// const words = splitMessage.split(" ")
+// console.log(words)
+
+// const message = "Hi! i am Mark"
+// console.log(message.split("!" && " "))
+
+// const friends = "Sasha, Induk Dima, Phone"
+// console.log(friends.split(""))
+
+//join метод робить з масива рядок
+// const test = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// const num = test.join("")
+// console.log(num)
+
+//indexOf метод дивитьтся під яким індексом знаходиться елемент в масиві
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// const indexNum = num.indexOf(5)
+// console.log(indexNum)
+
+//includes метод переваіряє чи є шуканий елемент у масиві
+const friends = ["Sasha", "Induk Dima", "Phone"]
+const isFriendInArray = friends.includes("Sasha")
+console.log(isFriendInArray)
+
+//pop метод видаляє останній елемент масиві
+friends.pop("Tima")
+console.log(friends)
+
+//push метод додає елемент у кінці масива 
+friends.push("Max")
+console.log(friends)
+
+//unshift метод додає елемент на початку масиву
+friends.push("start")
+console.log(friends)
+
+// метод shift - видяляє елемент на початку масива
+friends.shift()
+console.log(friends)

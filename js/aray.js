6ad0c5c3a2e8cd78da690ce4fcbd130cb7bd69c9
MarkUA <mark.d.nahorniak@gmail.com>
2024-11-03 10:12:24 +0200

    fix

--- js/aray.js
+++ js/aray.js
@@ -9,10 +9,10 @@
 // const lastIndex = friendQuality - 1
 // console.log(lastIndex)
 
-const numbers = [1, 3, 2, 4, 8]
-let total = 0
-for (let i = 0; i < numbers.length; i++){
-total += numbers[i]
-console.log(total)
+// const numbers = [1, 3, 2, 4, 8]
+// let total = 0
+// for (let i = 0; i < numbers.length; i++){
+// total += numbers[i]
+// console.log(total)
 // console.log(numbers[i])
-}
+// }
